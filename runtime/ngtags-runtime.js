0b5be64365164a7dcf44973d4e32d4d7e90655c9
David Rodenas <david.rodenas@gmail.com>
2015-01-31 20:32:46 +0100

    add support to custom transclude (transclude-to, transclude-id)

--- runtime/ngtags-runtime.js
+++ runtime/ngtags-runtime.js
@@ -1,13 +1,17 @@
 ;(function(angular) {
 
 	angular
-		.module('ngtagsRuntime', ['ngtagsId'])
+		.module('ngtagsRuntime', ['ngtagsId','ngtagsTranscludeId'])
 		.directive('id', idDirective);
 
 
 	angular
 		.module('ngtagsId', [])
 		.directive('id', idDirective);
+
+	angular
+		.module('ngtagsTranscludeId', [])
+		.directive('transcludeId', transcludeIdDirective);
 
 	function idDirective() {
 		var directive = {
@@ -21,6 +25,36 @@
 
 		return directive;
 	}
+
+	function transcludeIdDirective() {
+		var directive = {
+			restrict: 'A',
+			scope: true,
+			link: function (scope,element,attrs/*,ctr,transclude*/) {
+				var transcluded = scope.vm && scope.vm.$transcluded;
+
+				if (!transcluded) {
+					throw new Error('transcludeId: orphan\n'+
+						'Illegal use of transcludeId directive in the template! ' +
+						'No parent directive that requires a transclusion found. ');
+				}
+
+				var id = attrs.transcludeId;
+
+				var i, l, child;
+				for (i = 0, l = transcluded.length; i < l; i++) {
+					child = angular.element(transcluded[i]);
+					if (child.attr('transclude-to') === id) {
+						transcluded.splice(i, 1); i--; l--;
+						element.append(child);
+					}
+				} 
+			},
+		};
+
+		return directive;
+	}
+
 
 })(angular);
 
